a3dc59d4ab286da8a9e960c6764e717c874399ac
Andrei Vivar <drevivar2001@gmail.com>
2024-12-28 16:18:29 -0800

    updated files

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -43,6 +43,7 @@ const Contact = () => {
                         <AnimatedLetters letterClass={letterClass} strArray={['C','o','n','t','a','c','t',' ','M','e']} idx={15} />
                     </h1>
                     <p>Want to connect? Send me a message! I am always looking for connections and opportunities to network!</p>
+                    <p>This message will be sent to vivardre@gmail.com</p>
                     <div className='contact-form'>
                         <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
