d4f047503a67632f6d528b7190278e7d7f85299a
Andrei Vivar <drevivar2001@gmail.com>
2024-12-31 00:25:51 -0800

    updated information for the timeline compoonent

--- react-portfolio/src/components/Timeline/index.js
+++ react-portfolio/src/components/Timeline/index.js
@@ -3,22 +3,44 @@ import './index.scss';
 
 const timelineElements = [
     {
-        id: 1,
-        date: 'January 2023',
-        title: 'Project A',
-        description: 'Description for Project A.',
+        date: 'April 2016',
+        title: 'Moved to Canada',
+        location: '',
+        description: '',
     },
     {
-        id: 2,
-        date: 'March 2023',
-        title: 'Project B',
-        description: 'Description for Project B.',
+        date: 'Sept 2020',
+        title: 'Started Software Engineering at Thompson Rivers University',
+        location: 'Kamloops, BC',
+        description: 'Relevant Coursework: Data Structures, Algorithms, Software Development, Web Development, Database Management, Operating Systems, Computer Networks, Machine Learning, Artificial Intelligence, Software Testing, Software Architecture, Software Project Management',
     },
     {
-        id: 3,
-        date: 'June 2023',
-        title: 'Project C',
-        description: 'Description for Project C.',
+        date: 'May 2023 - Dec 2023',
+        title: 'Engineering Project Assistant at Houle Electric',
+        location: 'Fort St. John, BC',
+        description: 'Worked on BC Hydro\'s Site C Clean Energy Project',
+    },
+    {
+        date: 'Jan 2024 - Apr 2024',
+        title: 'Data Analyst at Thompson Rivers University',
+        location: 'Kamloops, BC',
+        description: 'involved in Data Analysis with TRU Employee Purchasing Card Transaction Database for the Fiscal Year 2024 Q4.',
+    },
+    {
+        date: 'Jun 2024 - Dec 2024',
+        title: 'Full-stack Database Developer at Kamloops Film Society',
+        location: 'Kamloops, BC',
+        description: 'Lead the development of a new database system for the Kamloops Film Society.',
+    },
+    {
+        date: 'Dec 2024 - Present',
+        title: 'AI/ML Engineer Intern via Riipen Level UP',
+        location: 'Remote, Canada',
+        description: `
+            Machine Learning Engineer Intern at AIModels: Creating solutions on Predictive Maintenance for Data Centers.<br />
+            AI Engineer Intern at ChmlTech Ltd.: Developing a Stock Analysis Tool using CrewAI and LangChain.<br />
+            AI Engineer Intern at ECOBASED.AI: Developed an AI-powered Regenerative Impact Estimator Tool that evaluates the economic, environmental, and social benefits of transitioning to regenerative projects.
+        `,
     },
 ];
 
@@ -31,7 +53,8 @@ const Timeline = () => {
                     <div className="timeline-date">{element.date}</div>
                     <div className="timeline-content">
                         <h3>{element.title}</h3>
-                        <p>{element.description}</p>
+                        <h4>{element.location}</h4>
+                        <p dangerouslySetInnerHTML={{ __html: element.description }}></p>
                     </div>
                 </div>
             ))}
